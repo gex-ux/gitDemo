1e130f0ca2229a2043f3b93de790482251946e80
gxy <2284131058@qq.com>
2020-07-04 20:48:03 +0800

    demo.js 新增一个数组去重函数

--- demo.js
+++ demo.js
@@ -16,3 +16,15 @@ function maxNum(arr){
 let a=[1,2,5,2,4,1,9];
 let o={id:12334,name:"小乖",age:"18"};
 console.log(maxNum(a));
+
+//数组的去重
+function arrayToHeavy(arr){
+    let arr1=[];
+    arr.forEach(item=>{
+        if(arr1.indexOf(item)===-1){
+            arr1.push(item);
+        }
+    })
+    return arr1;
+}
+console.log(arrayToHeavy(a));
